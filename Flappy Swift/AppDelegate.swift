5db18048e6a494d42c0154e41867a708298595d1
Joshua Raichur <hello@joshuaraichur.com>
2015-04-19 16:16:18 +0530

    flappy bird not tinder

--- Flappy Swift/AppDelegate.swift
+++ Flappy Swift/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  Tinder
+//  Flappy Swift
 //
-//  Created by Joshua Raichur on 4/15/15.
+//  Created by Joshua Raichur on 4/19/15.
 //  Copyright (c) 2015 Joshua Raichur. All rights reserved.
 //
 
--- Flappy SwiftTests/Flappy_SwiftTests.swift
+++ Flappy SwiftTests/Flappy_SwiftTests.swift
@@ -1,15 +1,15 @@
 //
-//  TinderTests.swift
-//  TinderTests
+//  Flappy_SwiftTests.swift
+//  Flappy SwiftTests
 //
-//  Created by Joshua Raichur on 4/15/15.
+//  Created by Joshua Raichur on 4/19/15.
 //  Copyright (c) 2015 Joshua Raichur. All rights reserved.
 //
 
 import UIKit
 import XCTest
 
-class TinderTests: XCTestCase {
+class Flappy_SwiftTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
