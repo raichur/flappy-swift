cf204ae3aff4425ccaf695875df2730f37fe7e14
Joshua Raichur <hello@joshuaraichur.com>
2015-04-20 22:34:08 +0530

    bg

--- Flappy Swift/GameScene.swift
+++ Flappy Swift/GameScene.swift
@@ -11,11 +11,13 @@ import SpriteKit
 class GameScene: SKScene {
     
     var bird = SKSpriteNode()
+    var bg = SKSpriteNode()
     
     override func didMoveToView(view: SKView) {
         
         var birdTexture = SKTexture(imageNamed: "flappy1.png")
         var birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+        var backgroundTexture = SKTexture(imageNamed: "bg.png")
         var animation = SKAction.animateWithTextures([birdTexture, birdTexture2], timePerFrame: 0.1)
         var makeBirdFlap = SKAction.repeatActionForever(animation)
         
@@ -23,7 +25,11 @@ class GameScene: SKScene {
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
         bird.runAction(makeBirdFlap)
         
+        bg = SKSpriteNode(texture: backgroundTexture)
+        bg.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        
         self.addChild(bird)
+        self.addChild(bg)
         
     }
     
